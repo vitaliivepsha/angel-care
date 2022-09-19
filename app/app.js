667d939d798649ce20c7035ae111e56e5e3512a3
vitaliivepsha <vitaly.vepsha@gmail.com>
2022-09-19 12:12:08 +0200

    skilled service - nursing

--- app/app.js
+++ app/app.js
@@ -5,6 +5,7 @@
 if (process.env.NODE_ENV !== 'production') {
     require('./assets/templates/layouts/index.html');
     require('./assets/templates/layouts/skilled_services.html');
+    require('./assets/templates/layouts/skilled_service_nursing.html');
     require('./assets/templates/layouts/about.html');
 }
 
